d4ac64c5880a6f2887fa2b918fbb65b074549009
Calvin Hsia <calvinh@microsoft.com>
2020-10-11 17:43:10 -0700

    refa

---
+++ MapFileDict/OutOfProc.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MapFileDict
+{
+    public enum Verbs
+    {
+        verbQuit, // len =1 byte: 0 args
+        verbString, // 
+        verbStringSharedMem,
+        verbSpeedTest,
+        verbRequestData, // len = 1 byte: 0 args
+        verbSendObjAndReferences, // a single obj and a list of it's references
+    }
+    public class OutOfProc
+    {
+        public int chunkSize;
+        public byte[] speedBuf;
+        public string pipeName;
+        public CancellationTokenSource cts;
+        public IntPtr mappedSection;
+        public uint sharedMapSize;
+        public string sharedFileMapName;
+        public MemoryMappedFile mmf;
+        public MemoryMappedViewAccessor mmfView;
+        public OutOfProc(int chunkSize = 100)
+        {
+            this.chunkSize = chunkSize;
+            sharedFileMapName = $"MapFileDict{Process.GetCurrentProcess().Id}\0";
+            sharedMapSize = 65536U;
+            mmf = MemoryMappedFile.CreateNew(
+               mapName: sharedFileMapName,
+               capacity: sharedMapSize,
+               access: MemoryMappedFileAccess.ReadWrite,
+               options: MemoryMappedFileOptions.None,
+               inheritability: HandleInheritability.None
+               );
+            mmfView = mmf.CreateViewAccessor(
+               offset: 0,
+               size: 0,
+               access: MemoryMappedFileAccess.ReadWrite);
+            mappedSection = mmfView.SafeMemoryMappedViewHandle.DangerousGetHandle();
+            cts = new CancellationTokenSource();
+            pipeName = "MyTestPipe";
+            speedBuf = new byte[chunkSize];
+        }
+        public Task CreateServer(string pipeName, IntPtr mappedSection, CancellationToken token)
+        {
+            var taskServer = Task.Run(async () =>
+             {
+                 try
+                 {
+                     Trace.WriteLine("Server: Starting ");
+                     using (var pipeServer = new NamedPipeServerStream(
+                         pipeName: pipeName,
+                         direction: PipeDirection.InOut,
+                         maxNumberOfServerInstances: 1,
+                         transmissionMode: PipeTransmissionMode.Message,
+                         options: PipeOptions.Asynchronous
+                         ))
+                     {
+                         await pipeServer.WaitForConnectionAsync(token);
+
+                         var buff = new byte[100];
+                         var receivedQuit = false;
+                         using (var ctsReg = token.Register(
+                                () => { pipeServer.Disconnect(); Trace.WriteLine("Cancel: disconnect pipe"); }))
+                         {
+                             while (!receivedQuit)
+                             {
+                                 if (token.IsCancellationRequested)
+                                 {
+                                     Trace.WriteLine("server: got cancel");
+                                     receivedQuit = true;
+                                 }
+                                 var nBytesRead = await pipeServer.ReadAsync(buff, 0, 1, token);
+                                 switch ((Verbs)buff[0])
+                                 {
+                                     case Verbs.verbQuit:
+                                         Trace.WriteLine($"Server got quit message");
+                                         receivedQuit = true;
+                                         break;
+                                     case Verbs.verbStringSharedMem:
+                                         var len = Marshal.ReadIntPtr(mappedSection);
+                                         var str = Marshal.PtrToStringAnsi(mappedSection + IntPtr.Size, len.ToInt32());
+                                         Trace.WriteLine($"Server: SharedMemStr {str}");
+                                         break;
+                                     case Verbs.verbString:
+                                         var lstBytes = new List<byte>();
+                                         while (!pipeServer.IsMessageComplete)
+                                         {
+                                             var byt = pipeServer.ReadByte();
+                                             lstBytes.Add((byte)byt);
+                                         }
+                                         //var bufLen = buff[1];
+                                         //var strbuf = new byte[bufLen];
+                                         //await pipeServer.ReadAsync(strbuf, 0, bufLen, cts.Token);
+                                         var strRead = Encoding.ASCII.GetString(lstBytes.ToArray());
+                                         Trace.WriteLine($"Server Got str {strRead}");
+                                         break;
+                                     case Verbs.verbRequestData:
+                                         var strB = Encoding.ASCII.GetBytes($"Server: {DateTime.Now}");
+                                         await pipeServer.WriteAsync(strB, 0, strB.Length);
+                                         break;
+                                     case Verbs.verbSpeedTest:
+                                         {
+                                             await pipeServer.ReadAsync(speedBuf, 0, chunkSize - 1);
+                                             Trace.WriteLine($"Server: got bytes {chunkSize:n0}");
+                                         }
+                                         break;
+                                 }
+                             }
+                         }
+                     }
+                 }
+                 catch (IOException ex)
+                 {
+                     Trace.WriteLine($"Server: IOException" + ex.ToString());
+                     throw;
+                 }
+                 catch (OperationCanceledException)
+                 {
+                     Trace.WriteLine("Server: cancelled");
+                 }
+                 Trace.WriteLine("Server: exiting servertask");
+
+             });
+            return taskServer;
+        }
+    }
+}
---
+++ MapFileDictTest/OOPCommTests.cs
@@ -0,0 +1,144 @@
+﻿using MapFileDict;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MapFileDictTest
+{
+    [TestClass]
+    public class OOPCommTests : MyTestBase
+    {
+        [TestMethod]
+        public async Task OOPTest()
+        {
+            Trace.WriteLine($"Start {nameof(OOPTest)}");
+            try
+            {
+                await Task.Yield();
+                var outputLogFile = TestContext.Properties[ContextPropertyLogFile] as string;
+                Trace.WriteLine($"Log = {outputLogFile}");
+                MyClassThatRunsIn32and64bit.CreateAndRun(outputLogFile);
+
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+        [TestMethod]
+        public async Task OOPPipeTesting()
+        {
+            var sharedFileMapName = $"MapFileDict{Process.GetCurrentProcess().Id}\0";
+            var cts = new CancellationTokenSource();
+            var pipeName = "MyTestPipe";
+            var sharedMapSize = 65536U;
+            using (var mmf = MemoryMappedFile.CreateNew(
+                mapName: sharedFileMapName,
+                capacity: sharedMapSize,
+                access: MemoryMappedFileAccess.ReadWrite,
+                options: MemoryMappedFileOptions.None,
+                inheritability: HandleInheritability.None
+                ))
+            using (var mmfView = mmf.CreateViewAccessor(
+                                offset: 0,
+                                size: 0,
+                                access: MemoryMappedFileAccess.ReadWrite))
+            {
+                var mappedSection = mmfView.SafeMemoryMappedViewHandle.DangerousGetHandle();
+                var oop = new OutOfProc();
+                await oop.CreateServer(pipeName, mappedSection, cts.Token);
+            }
+        }
+
+        [TestMethod]
+        public async Task PipeTesting()
+        {
+            var oop = new OutOfProc(chunkSize: 1024 * 1024 * 1024);
+            Trace.WriteLine($"Mapped Section {oop.mappedSection} 0x{oop.mappedSection.ToInt32():x8}");
+
+            var taskServer = oop.CreateServer(oop.pipeName, oop.mappedSection, oop.cts.Token);
+
+            var taskClient = Task.Run(async () =>
+            {
+                Trace.WriteLine("Starting Client");
+                using (var pipeClient = new NamedPipeClientStream(
+                    serverName: ".",
+                    pipeName: oop.pipeName,
+                    direction: PipeDirection.InOut,
+                    options: PipeOptions.Asynchronous))
+                {
+                    await pipeClient.ConnectAsync(oop.cts.Token);
+                    var verb = new byte[2] { 1, 1 };
+                    for (int i = 0; i < 5; i++)
+                    {
+                        {
+                            var strBuf = Encoding.ASCII.GetBytes($"MessageString {i}");
+                            var buf = new byte[strBuf.Length + 1];
+                            buf[0] = (byte)Verbs.verbString;
+                            Array.Copy(strBuf, 0, buf, 1, strBuf.Length);
+                            Trace.WriteLine("Client: sending message");
+                            await pipeClient.WriteAsync(buf, 0, buf.Length);
+                            Trace.WriteLine($"Client: sent message..requesting data");
+                        }
+                        {
+                            var strBuf = Encoding.ASCII.GetBytes($"StrSharedMem {i}");
+                            verb[0] = (byte)Verbs.verbStringSharedMem;
+                            Marshal.WriteInt32(oop.mappedSection, strBuf.Length);
+                            Marshal.Copy(strBuf, 0, oop.mappedSection + IntPtr.Size, strBuf.Length);
+                            await pipeClient.WriteAsync(verb, 0, 1);
+                        }
+                        {
+                            verb[0] = (byte)Verbs.verbRequestData;
+                            await pipeClient.WriteAsync(verb, 0, 1);
+                            var bufReq = new byte[100];
+                            var buflen = await pipeClient.ReadAsync(bufReq, 0, bufReq.Length);
+                            var readStr = Encoding.ASCII.GetString(bufReq, 0, buflen);
+                            Trace.WriteLine($"Client req data from server: {readStr}");
+                        }
+                    }
+                    {
+                        // speedtest
+                        var nIter = 10;
+                        var bufSpeed = new byte[oop.chunkSize];
+                        bufSpeed[0] = (byte)Verbs.verbSpeedTest;
+                        var sw = Stopwatch.StartNew();
+                        for (int iter = 0; iter < nIter; iter++)
+                        {
+                            Trace.WriteLine($"Sending chunk {iter}");
+                            await pipeClient.WriteAsync(bufSpeed, 0, bufSpeed.Length);
+                        }
+                        var bps = (double)oop.chunkSize * nIter / sw.Elapsed.TotalSeconds;
+                        Trace.WriteLine($"BytesPerSec = {bps:n0}"); // 1.4 G/Sec
+                    }
+
+                    Trace.WriteLine($"Client: sending quit");
+                    verb[0] = (byte)Verbs.verbQuit;
+                    await pipeClient.WriteAsync(verb, 0, 1);
+                }
+            });
+
+            var tskDelay = Task.Delay(TimeSpan.FromSeconds(Debugger.IsAttached ? 3000 : 13));
+            await Task.WhenAny(new[] { tskDelay, taskClient});
+            if (tskDelay.IsCompleted)
+            {
+                Trace.WriteLine($"Delay completed: cancelling server");
+                oop.cts.Cancel();
+                await taskServer;
+            }
+            oop.mmfView.Dispose();
+            oop.mmf.Dispose();
+            Trace.WriteLine($"Done");
+        }
+    }
+}
