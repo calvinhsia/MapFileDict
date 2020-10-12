4299fcb4104c9323b4dcb393aea7a0257c0a1be3
Calvin Hsia <calvinh@microsoft.com>
2020-10-12 11:25:45 -0700

    oop consapp

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,21 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace ConsoleAppTest
 {
     class Program
     {
-        static void Main(string[] args)s
+        static void Main(string[] args)
         {
-            doMyAsyncMethod();
+            var myMain = new Program();
+            myMain.DoMainAsync(args).Wait();
         }
 
-        private static async Task doMyAsyncMethod()
+        private async Task DoMainAsync(string[] args)
         {
-            await Task.Delay(TimeSpan.FromSeconds(3));
+            //            doMyAsyncMethod().Wait();
+            var tcsStaThread = new TaskCompletionSource<int>();
+            var execContext = CreateExecutionContext(tcsStaThread);
+            await execContext.Dispatcher.InvokeAsync(async () =>
+            {
+                Trace.WriteLine("delay start");
+                await doMyAsyncMethod();
+                Trace.WriteLine("delay done");
+
+            });
+            Trace.WriteLine("done");
+        }
+
+        private  async Task doMyAsyncMethod()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(4));
+        }
+        MyExecutionContext CreateExecutionContext(TaskCompletionSource<int> tcsStaThread)
+        {
+            const string Threadname = "MyStaThread";
+            var tcsGetExecutionContext = new TaskCompletionSource<MyExecutionContext>();
+
+            Trace.WriteLine($"Creating {Threadname}");
+            var myStaThread = new Thread(() =>
+            {
+                // Create the context, and install it:
+                Trace.WriteLine($"{Threadname} start");
+                var dispatcher = Dispatcher.CurrentDispatcher;
+                var syncContext = new DispatcherSynchronizationContext(dispatcher);
+
+                SynchronizationContext.SetSynchronizationContext(syncContext);
+
+                tcsGetExecutionContext.SetResult(new MyExecutionContext
+                {
+                    DispatcherSynchronizationContext = syncContext,
+                    Dispatcher = dispatcher
+                });
+
+                // Start the Dispatcher Processing
+                Trace.WriteLine($"MyStaThread before Dispatcher.run");
+                Dispatcher.Run();
+                Trace.WriteLine($"MyStaThread After Dispatcher.run");
+                tcsStaThread.SetResult(0);
+            });
+
+//            myStaThread.SetApartmentState(ApartmentState.STA);
+            myStaThread.Name = Threadname;
+            myStaThread.Start();
+            Trace.WriteLine($"Starting {Threadname}");
+            return tcsGetExecutionContext.Task.Result;
+        }
+
+        public class MyExecutionContext
+        {
+            public DispatcherSynchronizationContext DispatcherSynchronizationContext { get; set; }
+            public Dispatcher Dispatcher { get; set; }
         }
     }
 }
